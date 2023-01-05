f83e360931276696fe8a7be6067256efc909b9c5
khanhnvtb <khanhtb.1012@gmail.com>
2023-01-05 09:56:41 +0700

    Commit by Khanhnvtb

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ from pyvi import ViTokenizer
 import pandas as pd
 import joblib
 import math
-
-sheet_name = ['Câu hỏi', 'Case', 'Lỗi', 'Triệu chứng']
-
-
-uniChars = "àáảãạâầấẩẫậăằắẳẵặèéẻẽẹêềếểễệđìíỉĩịòóỏõọôồốổỗộơờớởỡợùúủũụưừứửữựỳýỷỹỵÀÁẢÃẠÂẦẤẨẪẬĂẰẮẲẴẶÈÉẺẼẸÊỀẾỂỄỆĐÌÍỈĨỊÒÓỎÕỌÔỒỐỔỖỘƠỜỚỞỠỢÙÚỦŨỤƯỪỨỬỮỰỲÝỶỸỴÂĂĐÔƠƯ"
-unsignChars = "aaaaaaaaaaaaaaaaaeeeeeeeeeeediiiiiooooooooooooooooouuuuuuuuuuuyyyyyAAAAAAAAAAAAAAAAAEEEEEEEEEEEDIIIOOOOOOOOOOOOOOOOOOOUUUUUUUUUUUYYYYYAADOOU"
+import time
 
 
 def loaddicchar():
@@ -32,20 +27,21 @@ def convert_unicode(txt):
 
 
 def text_preprocess(document):
+    # đưa về chữ viết thường
+    document = document.lower()
     # chuẩn hóa unicode
     document = convert_unicode(document)
     # tách từ(từ đơn và từ ghép)
     document = ViTokenizer.tokenize(document)
-    # đưa về chữ viết thường
-    document = document.lower()
-    #loại bỏ ký tự đặc biệt
+    # loại bỏ ký tự đặc biệt
     document = re.sub(
         r'[^\s\wáàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệóòỏõọôốồổỗộơớờởỡợíìỉĩịúùủũụưứừửữựýỳỷỹỵđ_]', ' ', document)
     # xóa khoảng trắng thừa
     document = re.sub(r'\s+', ' ', document).strip()
     return document
 
-dict_signal_DH = {
+
+DICT_SIGNAL_DH = {
     'HD': 'Hoạt động',
     'TT': 'Tình trạng',
     'ND': 'Nhiệt độ',
@@ -58,7 +54,7 @@ dict_signal_DH = {
     'TH': 'Tín hiệu remote'
 }
 
-dict_signal_TL = {
+DICT_SIGNAL_TL = {
     'LL': 'Khả năng làm lạnh',
     'D': 'Đèn',
     'B': 'Block',
@@ -71,8 +67,8 @@ dict_signal_TL = {
     'M': 'Mùi'
 }
 
-dict_signal = {}
 
+# Tìm thuộc tính có độ hỗn loạn nhỏ nhất
 def cal_Entropy(df: pd.DataFrame):
     # print(df)
     total_cases = df.shape[0]
@@ -93,100 +89,144 @@ def cal_Entropy(df: pd.DataFrame):
         if (entropy_min == -1) or (entropy_min > entropy):
             entropy_min = entropy
             property_selected = property
+
     list_option = df[property_selected].value_counts().keys()
+
     return property_selected, entropy_min, list_option,
 
+
 def main():
-    print('Hệ thống: Bạn cần tư vấn về vấn đề gì?')
-    print('1. Điều hòa')
-    print('2. Tủ lạnh')
-    while True:
-        option = input("Người dùng: ").strip()
-        
-        if (option == '1'):
-            folder = "Điều hòa"
-            dict_signal = dict_signal_DH
-            break
-        elif (option == '2'):
-            folder = "Tủ lạnh"
-            dict_signal = dict_signal_TL
-            break
+    program = True
+    while program:
+        # Hỏi thiết bị người dùng cần tư vấn
+        while True:
+            print('\nHệ thống: Bạn cần tư vấn về vấn đề gì?')
+            print('1. Điều hòa')
+            print('2. Tủ lạnh')
+            print('(Chọn 1 trong 2 lựa chọn)')
+            input_user = input("\nNgười dùng: ").strip()
+
+            if (input_user == '1'):
+                folder = "Điều hòa"
+                dict_signal = DICT_SIGNAL_DH
+                break
+            elif (input_user == '2'):
+                folder = "Tủ lạnh"
+                dict_signal = DICT_SIGNAL_TL
+                break
+            else:
+                print('\nHệ thống: Vui lòng nhập chính xác 1 hoặc 2')
+
+        # Load dữ liệu
+        sheet_name = ['Câu hỏi', 'Case', 'Lỗi', 'Triệu chứng']
+        data = pd.read_excel(f"{folder}/data.xlsx",
+                             sheet_name=sheet_name)
+        cases = data['Case']
+        errors = data['Lỗi']
+        questions = data['Câu hỏi']
+        expression = data['Triệu chứng']
+
+        # Load model
+        model = joblib.load(f'{folder}/model.joblib')
+        vectorizer = joblib.load(open(f"{folder}/tfidf.pkl", "rb"))
+
+        # Hỏi vấn đề của người dùng
+        print(f'\nHệ thống: {folder} của bạn gặp vấn đề gì?')
+        input_user = input('\nNgười dùng: ').lower()
+        input_user = text_preprocess(input_user)
+        input_user = vectorizer.transform([input_user])
+
+        # Dự đoán mã triệu chứng
+        if (max(model.predict_proba(input_user)[0]) >= 0.5):
+            problem_predict = model.predict(input_user)[0]
+            property = dict_signal[problem_predict[:-2]]
+            expression_code = problem_predict
+            finish = False
+
+            # Đưa ra câu hỏi dựa vào vấn đề gặp phải
+            while (finish == False):
+                cases = cases[cases[property].str.contains(expression_code)].drop(
+                    [property], axis=1)
+                property, entropy, list_expression_code = cal_Entropy(cases)
+
+                # Liệt kê các biểu hiện có thể xảy ra với thuộc tính cần hỏi
+                list_expression_code = sorted(
+                    list_expression_code, key=lambda x: (len(x), x))
+                list_option = []
+                for index, expression_code in enumerate(list_expression_code):
+                    codes = expression_code.split(', ')
+                    option = ''
+                    for i, code in enumerate(codes):
+                        option += expression[expression['Mã']
+                                             == code].iloc[0]['Dấu hiệu']
+                        if (i < len(codes)-1):
+                            option += ', '
+                    option = option.lower().capitalize()
+                    list_option.append(option)
+
+                list_option.append('Trường hợp khác')
+
+                # Đưa ra câu hỏi và lựa chọn
+                while True:
+                    question = questions[questions['Tiêu chí']
+                                         == property].iloc[0]['Câu hỏi']
+                    print(f"\nHệ thống: {question}")
+                    for index, expression_code in enumerate(list_option):
+                        print(f'{index+1}. {expression_code}')
+
+                    input_user = input('\nNgười dùng: ')
+
+                    try:
+                        input_user = int(input_user)-1
+                        if input_user < index:
+                            expression_code = list_expression_code[input_user]
+
+                            if (entropy - int(entropy) == 0):
+                                error_code = cases[cases[property]
+                                                   == expression_code].iloc[0]['Lỗi']
+                                error = errors[errors['Mã'] == error_code]
+                                print(
+                                    f'\nHệ thống: Chẩn đoán lỗi: {error.iloc[0]["Lỗi"]}\n')
+                                print(
+                                    f'Hệ thống: Nguyên nhân:\n{error.iloc[0]["Nguyên nhân"]}\n')
+                                print(
+                                    f'Hệ thống: Cách khắc phục:\n{error.iloc[0]["Khắc phục"]}\n')
+                                print(
+                                    f'Hệ thống: Lời khuyên:\n{error.iloc[0]["Lời khuyên"]}\n')
+                                finish = True
+                            break
+                        elif input_user == index:
+                            print(
+                                '\nHệ thống: Vấn đề bạn gặp phải chưa có trong hệ thống, bạn có thể gọi điện thoại đến số 0123456789 để được tư vấn trực tiếp từ nhân viên kỹ thuật\n')
+                            finish = True
+                            break
+                        else:
+                            print('\nHệ thống: Bạn vui lòng nhập đúng các lựa chọn')
+
+                    except:
+                        print('\nHệ thống: Bạn vui lòng nhập đúng các lựa chọn')
         else:
-            print('Hệ thống: Vui lòng nhập chính xác 1 hoặc 2')
+            print('\nHệ thống: Vấn đề bạn gặp phải chưa có trong hệ thống, bạn gọi điện thoại đến số 0123456789 để được tư vấn trực tiếp từ nhân viên kỹ thuật\n')
 
-    data = pd.read_excel(f"{folder}/data.xlsx",
-                        sheet_name=sheet_name)
+        time.sleep(2)
 
-    model = joblib.load(f'{folder}/model.joblib')
-    vectorizer = joblib.load(open(f"{folder}/tfidf.pkl", "rb"))
+        # Hỏi người dùng có tiếp tục tư vấn
+        while True:
+            print('Hệ thống: Bạn muốn tiếp tục tư vấn không?')
+            print('1. Có ')
+            print('2. Không')
+            print('(Chọn 1 trong 2 lựa chọn)')
+            input_user = input("\nNgười dùng: ").strip()
 
-    cases = data['Case']
-    errors = data['Lỗi']
-    question = data['Câu hỏi']
-    expression = data['Triệu chứng']
+            if (input_user == '1'):
+                break
+            elif (input_user == '2'):
+                print("\nHệ thống: Chúc bạn một ngày vui vẻ")
+                program = False
+                break
+            else:
+                print('\nHệ thống: Vui lòng nhập chính xác 1 hoặc 2')
 
-    print(f'Hệ thống: {folder} của bạn gặp vấn đề gì?')
-    input_user = input('Người dùng: ').lower()
-    input_user = text_preprocess(input_user)
-    input_user = vectorizer.transform([input_user])
-    
-    if(max(model.predict_proba(input_user)[0]) >= 0.3):
-        problem_predict = model.predict(input_user)[0]
-        # print(problem_predict)
-    else:
-        # print(model.predict(input_user)[0])
-        print('Hệ thống: Lỗi của bạn hiện chưa có trong dữ liệu của chúng tôi')
-        return
-    
-    property = dict_signal[problem_predict[:-2]]
-    option = problem_predict
-    finish = False
 
-    while(finish == False):
-        cases = cases[cases[property]== option].drop([property], axis=1)
-        property, entropy, list_option_code = cal_Entropy(cases)
-        # print(f'{property}: ')
-        list_option_code = sorted(list_option_code)
-        list_option = []
-        for index, option in enumerate(list_option_code):
-            codes = option.split(', ')
-            output = ''
-            for i, code in enumerate(codes):
-                output += expression[expression['Mã'] == code].iloc[0]['Dấu hiệu']
-                if (i < len(codes)-1):
-                    output += ', '
-            list_option.append(output)
-
-        list_option.append('Trường hợp khác')
-
-        while True:          
-            question_system = question[question['Tiêu chí'] == property].iloc[0]['Câu hỏi']
-            print(
-                f"Hệ thống: {question_system}")
-            for index, option in enumerate(list_option):
-                print(f'{index+1}. {option}')
-
-            input_user = input('Người dùng: ')
-
-            try:
-                input_user = int(input_user)-1
-                if input_user < index:
-                    option = list_option_code[input_user]
-                    if (entropy - int(entropy) == 0):
-                        error_code = cases[cases[property] == option].iloc[0]['Lỗi']
-                        error = errors[errors['Mã'] == error_code]
-                        print(f'Hệ thống: Chẩn đoán lỗi: {error.iloc[0]["Lỗi"]}\n')
-                        print(f'Hệ thống: Nguyên nhân:\n{error.iloc[0]["Nguyên nhân"]}\n')
-                        print(f'Hệ thống: Cách khắc phục:\n{error.iloc[0]["Khắc phục"]}\n')
-                        print(f'Hệ thống: Lời khuyên:\n{error.iloc[0]["Lời khuyên"]}\n')
-                        finish = True
-                    break
-                elif input_user == index:
-                    print('Hệ thống: Vấn đề bạn gặp phải chưa có trong hệ thống, bạn có thể mang qua cửa hàng hoặc gọi điện thoại đến số 0123456789 để được tư vấn trực tiếp từ nhân viên kỹ thuật.')
-                    finish = True
-                    break
-                else :
-                    print('Hệ thống: Bạn vui lòng nhập đúng các lựa chọn')
-            except:
-                print('Hệ thống: Bạn vui lòng nhập đúng các lựa chọn')
-main()
+if __name__ == '__main__':
+    main()
